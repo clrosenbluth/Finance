a49ba6768294a8746f1322a500b6da27b30ce8dd
clrosenbluth <clrosenbluth@gmail.com>
2022-12-29 18:34:31 -0500

    removed unnecessary function from rate calculator

--- src/main/java/RateCalculator.java
+++ src/main/java/RateCalculator.java
@@ -3,12 +3,7 @@ public class RateCalculator
     public RateCalculator()
     {}
 
-    public Float getLinearRate (Float spot, Float futureRate, Integer days)
-    {
-        return (365 / days) * ((futureRate / spot) - 1);
-    }
-
-    public Float getContRate (Float futureRate, Integer days)
+    public Float getContinuousRate (Float futureRate, Integer days)
     {
         return (float) (365 * Math.log(futureRate)) / days;
     }
