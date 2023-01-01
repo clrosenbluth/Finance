c63e948923bd212f796271bea8714332a33ec375
SaraYaffe <syaffe@student.touro.edu>
2023-01-01 15:50:20 -0500

    renamed columns

--- src/main/java/frame/panels/PNLPanel.java
+++ src/main/java/frame/panels/PNLPanel.java
@@ -6,6 +6,10 @@ import java.awt.*;
 
 public class PNLPanel extends JPanel{
     private final String[] currencyTypes;
+
+    private double overallPNL = 0.0; //since initial investment or overall pnl within given time frame
+    private double investment = 10000; //holds value of what is remaining from original investment
+
     public PNLPanel(String[] currencyTypes){
         this.currencyTypes = currencyTypes;
 
@@ -18,13 +22,12 @@ public class PNLPanel extends JPanel{
     }
 
     private void fillPNLTab() {
-        String[] columnNames = {"Close",
+        String[] columnNames = {
                 "Date",
-                "Rate",
-                "Foreign Currency Details",
-                "Total USD Value in FX",
-                "PnL for all Currencies",
-                "Remaining USD Value from Original Investment"
+                "Total FX Position",
+                "Remaining USD Value from Original Investment",
+                "PnL",
+
         };
         int rows = 5;
         DefaultTableModel model = new DefaultTableModel(rows, columnNames.length);
