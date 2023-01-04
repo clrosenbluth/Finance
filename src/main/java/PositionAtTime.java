a788fed3b6154564b73cb54ac52a25cf5bb61c39
clrosenbluth <clrosenbluth@gmail.com>
2023-01-04 06:58:28 -0500

    added tests

--- src/main/java/PositionAtTime.java
+++ src/main/java/PositionAtTime.java
@@ -23,54 +23,62 @@ public class PositionAtTime
     public HashMap<String, Double> getPositionAtTime(LocalDate date) throws SQLException
     {
         HashMap<String, Double> positions = new HashMap<>();
+        positions.put("USD", initialUSD); // adding separately because not included in currency list
         for (String curr : currencies)
         {
-            Double amt = curr.equals("USD") ? initialUSD : 0.0;
-            positions.put(curr, amt);
+            positions.put(curr, 0.0);
         }
 
         ArrayList<String[]> records = sp.getAllTransactions();
         // form: date, vendor, quantity, type, currency, rate, maturity
         // quantity: amount in FX
         // rate: FX per dollar
-        for (String[] record : records)
+        if (records != null)
         {
-            String t_date = record[0];
-            if (LocalDate.parse(t_date).isAfter(date))
+            for (String[] record : records)
             {
-                continue;
+                String t_date = record[0];
+                if (LocalDate.parse(t_date).isAfter(date))
+                {
+                    continue;
+                }
+
+                // otherwise:
+                String type = record[3];
+                String maturityD = record[6];
+                LocalDate maturityLD = maturityD == null || maturityD.equals("") ?
+                        null :
+                        LocalDate.parse(maturityD);
+
+                boolean isSpot = type.equals("spot");
+                boolean isGoodFuture = type.equals("future")
+                        && maturityD != null
+                        && maturityLD.isAfter(date);
+                if (isSpot || isGoodFuture)
+                {
+                    Double quantity = Double.parseDouble(record[2]);
+                    String currency = record[4];
+                    Double rate = Double.parseDouble(record[5]);
+
+                    // remove (or add) the number of dollars
+                    double base = positions.get("USD");
+                    double fxInDollars = quantity / rate;
+                    // make sure it has 2 decimal places
+                    fxInDollars = fxInDollars*100;
+                    fxInDollars = (double)((int) fxInDollars);
+                    fxInDollars = fxInDollars /100;
+
+                    double newBase = base - fxInDollars;
+                    positions.put("USD", newBase);
+
+                    // add (or remove) the number of FX units
+                    Double fx = positions.get(currency);
+                    Double newFX = fx + quantity;
+                    positions.put(currency, newFX);
+                }
+                //todo: forwards: nothing is exchanged until the maturity date
+                // value accumulates because the promise can be sold
             }
-
-            // otherwise:
-            String type = record[3];
-            String maturityD = record[6];
-            LocalDate maturityLD = maturityD == null ?
-                    null :
-                    LocalDate.parse(maturityD);
-
-            boolean isSpot = type.equals("spot");
-            boolean isGoodFuture = type.equals("future")
-                    && maturityD != null
-                    && maturityLD.isAfter(date);
-            if (isSpot || isGoodFuture)
-            {
-                Double quantity = Double.parseDouble(record[2]);
-                String currency = record[4];
-                Double rate = Double.parseDouble(record[5]);
-
-                // remove (or add) the number of dollars
-                double base = positions.get("USD");
-                double fxInDollars = quantity * rate;
-                double newBase = base - fxInDollars;
-                positions.put("USD", newBase);
-
-                // add (or remove) the number of FX units
-                Double fx = positions.get(currency);
-                Double newFX = fx + quantity;
-                positions.put(currency, newFX);
-            }
-            //todo: forwards: nothing is exchanged until the maturity date
-            // value accumulates because the promise can be sold
         }
         return positions;
     }
