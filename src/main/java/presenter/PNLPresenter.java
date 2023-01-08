8d59a2cd848234ae3fecf6d9eb931b96deab5eed
SaraYaffe <syaffe@student.touro.edu>
2023-01-07 21:27:48 -0500

    pseudocode for pnl presenter...waiting on other merges

--- src/main/java/presenter/PNLPresenter.java
+++ src/main/java/presenter/PNLPresenter.java
@@ -1,5 +1,40 @@
 package presenter;
 
+import java.time.LocalDate;
+import java.util.HashMap;
+
 public class PNLPresenter {
+    //private PositionAtTime positionAtTime
+    private LocalDate firstDate;
+
+    private LocalDate lastDate;
+    private HashMap<LocalDate, double[]> PNLTable;
+
+    public PNLPresenter()//add in PositionAtTime as parameter
+    {
+        //this.positionAtTime = positionAtTime
+        //this.firstDate = 1st transaction date from sp in the PositionAtTime
+        //this.lastDate = most recent
+        PNLTable = new HashMap<>();
+    }
+
+    public void setLastDate(LocalDate lastDate) {
+        this.lastDate = lastDate;
+    }
+
+    //for each date from first to last date
+        //HashMap<String, Double> positions = store positionAtTime.getPositionAtTime in variable
+
+        //pnlTableValues = new double[3]
+        //pnlTableValues[0] = traverse through positions and calculate values(units of fx)*rate from that days close or current if today(see lines 63-69 in PositionAtTime)
+        //pnlTableValues[1] = value of "USD" from positions;
+        //pnlTableValues[2] = (pnlTableValues[0]+pnlTableValues[1]) - (previous)(pnlTableValues[0]+pnlTableValues[1]) OR 0 if first transaction
+
+        //PNLTable.put(date, pnlTableValues);
+
+
+   /* public double getOverallPnL(){
+        ((lastDate)pnlTableValues[0]+pnlTableValues[1]) - (firstDate)(pnlTableValues[0]+pnlTableValues[1])
+    }*/
 
 }
