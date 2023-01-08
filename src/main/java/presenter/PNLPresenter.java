6c5b39e5fd2e9d6fc5857f55dcb9a7d50c4e0960
SaraYaffe <syaffe@student.touro.edu>
2023-01-08 01:21:15 -0500

    get information needed to populate pnl panel

--- src/main/java/presenter/PNLPresenter.java
+++ src/main/java/presenter/PNLPresenter.java
@@ -2,14 +2,31 @@ package presenter;
 
 import calculations.PNLTableValues;
 
+
 public class PNLPresenter {
     private PNLTableValues tableValues;
+    private String lastDate;
+    private String firstDate;
 
-    public PNLPresenter (PNLTableValues tableValues) {
+    public PNLPresenter(PNLTableValues tableValues, String lastDate, String firstDate) {
         this.tableValues = tableValues;
+        this.firstDate = firstDate;
+        this.lastDate = lastDate;
     }
 
-    /* public double getOverallPnL(){
-        ((lastDate)pnlTableValues[0]+pnlTableValues[1]) - (firstDate)(pnlTableValues[0]+pnlTableValues[1])
-    }*/
+    public void calculatePNLTable() {
+        //TODO: implement
+    }
+
+
+    public double getOverallPnL() {
+        double overallPNL = 0.0;
+
+        double[] rowOfLastDate = tableValues.getPNLTable().get(lastDate);
+        double[] rowOfFirstDate = tableValues.getPNLTable().get(firstDate);
+        overallPNL =
+                (rowOfLastDate[0] + rowOfLastDate[1]) - (rowOfFirstDate[0] + rowOfFirstDate[1]);
+
+        return overallPNL;
+    }
 }
